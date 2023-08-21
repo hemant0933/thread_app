c3eb68d42334dd0d3b13aba4bb02bc2ab6d2ad05
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-08-21 11:20:51 +0530

    first commit

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,62 @@
+// import UserCard from "@/components/cards/UserCard";
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+    const user = await currentUser();
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
+    //getActivity
+
+    const activity = await getActivity(userInfo._id);
+
+    return (
+      <section>
+        <h1 className="head-text mb-10">
+          Activity
+        </h1>
+        <section className="mt-10 flex flex-col">
+          {
+            activity.length > 0 ? (
+              <>
+              {
+                activity.map((activity) => (
+                  <Link key={activity._id} href={`thread/${activity.parentId}`}>
+                    <article className="activity-card">
+                      <Image
+                        src={activity.author.image}
+                        width={20}
+                        height={20}
+                        alt="Profile Photo"
+                        className="rounded-full object-cover"
+                      />
+                      <p className="!text-small-regular text-light-1">
+                        <span className="mr-1 text-primary-500">
+                          {activity.author.name}
+                        </span>
+                        replied to your thread
+                      </p>
+                    </article>
+                  </Link>
+                ))}
+              </>
+            ):
+            (
+              <p className="!text-base-regular text-light-3">
+                No activity yet
+              </p>
+            )
+          }
+        </section>
+      </section>
+    )
+  }
+  
+  export default Page;
+  
